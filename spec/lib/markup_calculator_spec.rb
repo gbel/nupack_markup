13b940837d136c0b97fc66aa6df0de03c9d65113
Gabe De Oliveira <gabriel@olivox.ca>
2014-12-06 10:48:42 -0500

    DRY on rspec file

--- spec/lib/markup_calculator_spec.rb
+++ spec/lib/markup_calculator_spec.rb
@@ -1,40 +1,35 @@
 require 'spec_helper'
 
 describe MarkupCalc do
-    it "should raise and error if missing required info" do 
+    let(:markup_foo) { MarkupCalc.new(:base_price => 123,
+                                      :category => 'foo',
+                                      :req_persons => 1)
+    }
+
+    let(:food_markup) { MarkupCalc.new(:base_price => 123,
+                                       :category => 'Food',
+                                       :req_persons => 2)
+    }
+
+    it "should raise an error if missing required info" do 
         expect{MarkupCalc.new({})}.to raise_error(MarkupCalcArgError)
     end
     it "initialize the class with required info" do
-        markup = MarkupCalc.new(:base_price => 123,
-                                :category => 'foo',
-                                :req_persons => 1)
-        expect(markup.base_price).to eq(123)
-        expect(markup.category).to eq('foo')
-        expect(markup.req_persons).to eq(1)
+        expect(markup_foo.base_price).to eq(123)
+        expect(markup_foo.category).to eq('foo')
+        expect(markup_foo.req_persons).to eq(1)
     end
     it "category parameter to be marked up should be lower cased symbol" do
-        markup = MarkupCalc.new(:base_price => 123,
-                                :category => 'Food',
-                                :req_persons => 1)
-        expect(markup.send(:sanitized_category)).to be(:food)
+        expect(food_markup.send(:sanitized_category)).to be(:food)
     end
     it "should add the flat markup of 5%" do 
-        markup = MarkupCalc.new(:base_price => 123,
-                                :category => 'Food',
-                                :req_persons => 1)
-        expect(markup.send(:apply_flat_markup)).to eq(129.15)
+        expect(food_markup.send(:apply_flat_markup)).to eq(129.15)
     end
     it "should add markup for required persons" do
-        markup = MarkupCalc.new(:base_price => 123,
-                                :category => 'Food',
-                                :req_persons => 2)
-        expect(markup.send(:person_markup_total)).to eq(0.024)
+        expect(food_markup.send(:person_markup_total)).to eq(0.024)
     end
     it "should add markup for predefined categories ('electronics', 'food', 'drugs')" do
-        markup = MarkupCalc.new(:base_price => 123,
-                                :category => 'Food',
-                                :req_persons => 2)
-        expect(markup.send(:category_markup_total)).to eq(0.13)
+        expect(food_markup.send(:category_markup_total)).to eq(0.13)
     end
     it "should return repackaging estimated cost" do
         input_1 = MarkupCalc.new(:base_price => 1299.99,
