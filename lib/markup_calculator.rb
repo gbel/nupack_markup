f8bab2aed07527a4a376b838d6e1fa95666ff38d
Gabe De Oliveira <gabriel@olivox.ca>
2014-12-06 01:00:52 -0500

    Added MarkupCalc initialization

--- lib/markup_calculator.rb
+++ lib/markup_calculator.rb
@@ -1,2 +1,24 @@
+MarkupCalcArgError = Class.new(ArgumentError)
+
 class MarkupCalc
+    attr_reader :base_price, :materials, :req_persons
+
+    def initialize(**kwargs)
+        validate_req_params(kwargs)
+        @base_price = kwargs[:base_price]
+        @materials = kwargs[:materials]
+        @req_persons = kwargs[:req_persons]
+    end
+
+    def validate_req_params(args = {})
+        unless args[:base_price]
+            raise MarkupCalcArgError.new("Missing Base Price")
+        end
+        unless args[:materials]
+            raise MarkupCalcArgError.new("Missing Materials")
+        end
+        unless args[:req_persons]
+            raise MarkupCalcArgError.new("Missing number of workers")
+        end
+    end
 end
--- spec/lib/markup_calculator_spec.rb
+++ spec/lib/markup_calculator_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe MarkupCalc do
-
+    describe "#initialize" do
+        it "should raise and error if missing required info" do 
+            expect{MarkupCalc.new({})}.to raise_error(MarkupCalcArgError)
+        end
+        it "initialize the class with required info" do
+            markup = MarkupCalc.new(:base_price => 123,
+                                    :materials => 'foo',
+                                    :req_persons => 1)
+            expect(markup.base_price).to eq(123)
+            expect(markup.materials).to eq('foo')
+            expect(markup.req_persons).to eq(1)
+        end
+    end
 end
