dd1fc0ea45aaace8a0ff54faacb1925e58a1aae8
Gabe De Oliveira <gabriel@olivox.ca>
2014-12-06 02:13:06 -0500

    Renamed materials to category as it makes more sense and added a sanitization method for it

--- lib/markup_calculator.rb
+++ lib/markup_calculator.rb
@@ -1,21 +1,25 @@
 MarkupCalcArgError = Class.new(ArgumentError)
 
 class MarkupCalc
-    attr_reader :base_price, :materials, :req_persons
+    attr_reader :base_price, :category, :req_persons
 
     def initialize(**kwargs)
         validate_req_params(kwargs)
         @base_price = kwargs[:base_price]
-        @materials = kwargs[:materials]
+        @category = kwargs[:category]
         @req_persons = kwargs[:req_persons]
+    end
+
+    def sanitized_category()
+        return category.to_sym.downcase
     end
 
     def validate_req_params(args = {})
         unless args[:base_price]
             raise MarkupCalcArgError.new("Missing Base Price")
         end
-        unless args[:materials]
-            raise MarkupCalcArgError.new("Missing Materials")
+        unless args[:category]
+            raise MarkupCalcArgError.new("Missing Category")
         end
         unless args[:req_persons]
             raise MarkupCalcArgError.new("Missing number of workers")
--- spec/lib/markup_calculator_spec.rb
+++ spec/lib/markup_calculator_spec.rb
@@ -7,11 +7,17 @@ describe MarkupCalc do
         end
         it "initialize the class with required info" do
             markup = MarkupCalc.new(:base_price => 123,
-                                    :materials => 'foo',
+                                    :category => 'foo',
                                     :req_persons => 1)
             expect(markup.base_price).to eq(123)
-            expect(markup.materials).to eq('foo')
+            expect(markup.category).to eq('foo')
             expect(markup.req_persons).to eq(1)
+        end
+        it "category parameter to be marked up should be lower cased symbol" do
+            markup = MarkupCalc.new(:base_price => 123,
+                                    :category => 'Food',
+                                    :req_persons => 1)
+            expect(markup.sanitized_category()).to be(:food)
         end
     end
 end
